c1d33c56cc0483efa8e37320580d5182c99533f6
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-08-25 17:07:33 -0300

    Binary Tree Setup
    Binary Tree setup

---
+++ binary tree/1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct tree{
+	int info;
+	struct tree* left;
+	struct tree* right;	
+};
+typedef struct tree Tree;
+
+Tree* tree_create_empty(){
+	return NULL;
+}
+
+Tree* tree_create(){
+	
+}
+int main(){
+	
+}
