e80a7ebd447c6ce59d1f354903af51cf241c112b
Rafael Faustini Costa de Oliveira <contato@rafaelfaustini.com.br>
2018-08-28 10:41:35 -0300

    Update

--- binary tree/Data Structures 2.cpp
+++ binary tree/Data Structures 2.cpp
@@ -8,9 +8,17 @@ struct arv{
 };
 typedef struct arv Arv;
 
-Arv* arv_cria (void){
+Arv* cria (void){
 	return NULL;
 } 
+
+int vazia(Arv* a){
+	if(a==NULL){
+		return 1
+	}
+	return 0;
+}
+
 
 
 void insere(char ele, Arv* sae, Arv* sad){
@@ -23,5 +31,19 @@ void insere(char ele, Arv* sae, Arv* sad){
 
 
 int main(){
-	
+
+Arv* a1 = insere('J',cria(),cria());
+Arv* a2 = insere('I',a1,cria());
+Arv* a3 = insere('M',cria(),cria());
+Arv* a4 = insere('H',a3,a2);
+Arv* a5 = insere('D',cria(),a1);
+Arv* a6 = insere('C',cria(),a1);
+Arv* a7 = insere('G',cria(),a1);
+Arv* a8 = insere('I',cria(),a1);
+Arv* a9 = insere('I',cria(),a1);
+Arv* a10 = insere('I',cria(),a1);
+Arv* a11 = insere('I',cria(),a1);
+Arv* a12 = insere('I',cria(),a1);,
+Arv* a13 = insere('I',cria(),a1);
+
 }
