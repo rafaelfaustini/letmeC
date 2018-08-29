948704a83bddf983ab543d6d7369c19af4d46c96
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-08-29 06:15:46 -0300

    Update

--- binary tree/Data Structures 2.cpp
+++ binary tree/Data Structures 2.cpp
@@ -5,45 +5,74 @@ struct arv{
 	char info;
 	struct arv *esq;
 	struct arv *dir;
-};
-typedef struct arv Arv;
+};typedef struct arv Arv;
 
-Arv* cria (void){
+Arv* cria(){
 	return NULL;
-} 
+}
 
 int vazia(Arv* a){
 	if(a==NULL){
-		return 1
+		return 1;
 	}
 	return 0;
 }
 
+Arv* adiciona(char info,Arv* sae,Arv* sad)
+{
+Arv* p=(Arv*)malloc(sizeof(Arv));
+p->info = info;
+p->esq = sae;
+p->dir = sad;
+return p;
+}
 
-
-void insere(char ele, Arv* sae, Arv* sad){
-	
-	Arv *a = (Arv*)malloc(sizeof(arv));
-	p->info ele;
-	p->dir = sae;
-	p->esq = sad;
+void simetrica(Arv* a)
+{
+if (!vazia(a)){
+printf("%c ", a->info); 
+simetrica(a->esq); 
+simetrica(a->dir); 
+}
+}
+void pre_ordem (Arv* a)
+{
+if (!vazia(a)){
+pre_ordem(a->esq); 
+printf("%c ", a->info);
+pre_ordem(a->dir); 
+}
+}
+void pos_ordem (Arv* a)
+{
+if (!vazia(a)){
+pos_ordem(a->esq); 
+pos_ordem(a->dir);
+printf("%c ", a->info); 
+}
 }
 
 
 int main(){
+Arv* a=adiciona('J',cria(),cria());
+Arv* a1=adiciona('I',a,cria());
+Arv* a2=adiciona('M',cria(),cria());
+Arv* a3=adiciona('H',a1,a2);
+Arv* a4=adiciona('D',cria(),a3);
+Arv* a5=adiciona('G',cria(),cria());
+Arv* a6=adiciona('C',a4,a5);
+Arv* a7=adiciona('L',cria(),cria());
+Arv* a8=adiciona('K',a7,cria());
+Arv* a9=adiciona('F',cria(),cria());
+Arv* a10=adiciona('E',a9,a8);
+Arv* a11=adiciona('B',a6,a10);
+Arv* a12=adiciona('A',cria(),a11);
 
-Arv* a1 = insere('J',cria(),cria());
-Arv* a2 = insere('I',a1,cria());
-Arv* a3 = insere('M',cria(),cria());
-Arv* a4 = insere('H',a3,a2);
-Arv* a5 = insere('D',cria(),a1);
-Arv* a6 = insere('C',cria(),a1);
-Arv* a7 = insere('G',cria(),a1);
-Arv* a8 = insere('I',cria(),a1);
-Arv* a9 = insere('I',cria(),a1);
-Arv* a10 = insere('I',cria(),a1);
-Arv* a11 = insere('I',cria(),a1);
-Arv* a12 = insere('I',cria(),a1);,
-Arv* a13 = insere('I',cria(),a1);
+simetrica(a12);
+printf("\n");
+pre_ordem(a12);
+printf("\n");
+pos_ordem(a12);
 
+return 0;
 }
