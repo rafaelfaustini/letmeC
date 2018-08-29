0be36ed982e2e9a9401fdbb5794ebb48145fb294
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-08-29 15:15:19 -0300

    Presentation Changes

--- binary tree/Data Structures 2.cpp
+++ binary tree/Data Structures 2.cpp
@@ -53,21 +53,22 @@ printf("%c ", a->info);
 }
 
 
+
 int main(){
 // J
-Arv* a=adiciona('J',cria(),cria());
+Arv* a=adiciona('J',cria() ,cria() );
 // I
-Arv* a1=adiciona('I', a, cria());
+Arv* a1=adiciona('I', cria(), a );
 // M
 Arv* a2=adiciona('M', cria() , cria() );
 // H
-Arv* a3=adiciona('H',a2,a1);
+Arv* a3=adiciona('H',a2 ,a1 );
 // D
-Arv* a4=adiciona('D',a3,cria());
+Arv* a4=adiciona('D',a3 ,cria() );
 // G
-Arv* a5=adiciona('G',cria(),cria());
+Arv* a5=adiciona('G',cria() ,cria() );
 // C
-Arv* a6=adiciona('C',a5,a4);
+Arv* a6=adiciona('C',a5 ,a4 );
 
 
 // L
@@ -77,17 +78,28 @@ Arv* a8=adiciona('K',cria(),a7);
 // F
 Arv* a9=adiciona('F',cria(),cria());
 // E
-Arv* a10=adiciona('E',a8,a9);
-// B
-Arv* a11=adiciona('B',a10,a6);
-// A
-Arv* a12=adiciona('A',a11,cria());
+Arv* a10=adiciona('E',a8 ,a9 );
 
+
+// B
+Arv* a11=adiciona('B',a10 ,a6 );
+
+
+// Raiz A
+Arv* a12=adiciona('A',a11 ,cria() );
+
+
+printf("\n\t\tInOrder \n\t");
 simetrica(a12);
-printf("\n");
+printf("\n\n");
+
+printf("\t\tPreOrder \n\t");
 pre_ordem(a12);
-printf("\n");
+printf("\n\n");
+
+printf("\t\tPostOrder \n\t");
 pos_ordem(a12);
+printf("\n\n");
 
 return 0;
 }
